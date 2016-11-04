0bd4b426391bc5c41c376cbd5721517121dc0896
ashrafkandil <ashraf.kandil@gmail.com>
2016-11-04 07:04:56 -0400

    start with windows app

---
+++ Windows/ConsoleApp1/src/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("My First .Net Core App");
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+            
+            Environment.Exit(0);
+        }
+    }
+}
